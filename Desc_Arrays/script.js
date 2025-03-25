26c8703776c84fbcedfad4848dc1f7ee8892bf86
gabrielamaiia01 <gabrielamaiia01@gmail.com>
2025-03-25 19:13:53 -0300

    resolução de exercícios

--- Desc_Arrays/script.js
+++ Desc_Arrays/script.js
@@ -1,7 +1,7 @@
 // Definição de um objeto "pessoa"
 let pessoa = {
     nome: "Gabriela", 
-    idade: 19, 
+    //idade: 19, 
     sobrenome: "Maia", 
     profissao: "DEV" 
 };
@@ -27,3 +27,52 @@ console.log("Terceira pessoa: " + terceiraPessoa); // Maria
 // Outra forma de desestruturação, pegando diretamente pelo índice
 let [nome1] = nomes; // Pega o primeiro elemento do array
 console.log("Nome1: " + nome1); // Gabriela
+
+
+//Exercícios 
+let carro = {
+    marca: "Fiat", 
+    modelo: "Palio", 
+    ano: 2016
+}
+
+const { marca, modelo } = carro; 
+console.log(marca); //Fiat 
+
+let person = {
+    nome: "Gabriela", 
+    cargo: "Cronicamente Online", 
+    endereco: {
+        Rua: "Rua Bom Jesus",
+        Cidade: "Recife", 
+        Estado: "Pernambuco" 
+    }
+}
+                        //Desentruturação aninhada: Desestruturação para extrair cidade e estado diretamente do objeto endereco
+const {nome:nomeUsuario, cargo, endereco:{Rua,Cidade}} = person;
+console.log(nomeUsuario)
+console.log(Rua)
+
+let configuracao = {
+   tema: "Dark"
+}
+
+const {tema , modoEscuro = false } = configuracao
+console.log(tema) //dark 
+console.log(modoEscuro) //false 
+
+//Exercícios Array 
+let frutas = ["Maça", "banana", "uva"]; 
+const {0:Elemento1, 1:Elemento2} = frutas
+console.log(Elemento1, Elemento2)
+
+let numeros = [10, 20, 30, 40]; 
+const {2:terceiroNumero} = numeros; 
+console.log(terceiroNumero)
+
+let a = 5;
+let b = 10;
+
+[a, b] = [b, a]; // Troca de valores usando desestruturação
+
+

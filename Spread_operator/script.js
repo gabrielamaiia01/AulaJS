c9bc0d05d24cb2034d2b85d72053eaa0d04f7705
gabrielamaiia01 <gabrielamaiia01@gmail.com>
2025-03-27 14:27:26 -0300

    Exercícios Simples - Spread Operator

--- Spread_operator/script.js
+++ Spread_operator/script.js
@@ -49,3 +49,55 @@ novoobjeto({ nome2: "Gabriela", idade2: 12 });
 // Saída: { nome2: "Gabriela", idade2: 12, sobrenome: "Maia", cargo: "Desenvolvedora", status: "ativo" }
 // O objeto original foi expandido com novas propriedades
 
+//Exercícios
+let frutas1 = ["maçã", "banana", "uva"];
+let frutas2 = [...frutas1, "laranja", "manga"];
+console.log(frutas2);  
+// Saída esperada: ["maçã", "banana", "uva", "laranja", "manga"]
+
+let numero = [1, 2, 3];
+let letras = ["a", "b", "c"];
+
+// Seu código aqui
+let tudoJunto = [...numero, ...letras]
+
+console.log(tudoJunto);  
+// Saída esperada: [1, 2, 3, "a", "b", "c"]
+
+let original = ["azul", "verde", "amarelo"];
+
+// Seu código aqui
+let copia = [...original]
+
+console.log(copia); // Saída esperada: ["azul", "verde", "amarelo"]
+
+let pessoa1 = { nome: "Gabriela", idade: 20 };
+
+// Seu código aqui
+let pessoaCompleta = {
+    ...pessoa1,
+    cidade: "Recife",
+    cargo: "Desenvolvedora"
+}
+
+console.log(pessoaCompleta);  
+// Saída esperada: { nome: "Gabriela", idade: 20, cidade: "Recife", profissão: "Desenvolvedora" }
+
+//Exercicio
+function adicionarInfo(obj) {
+
+    let novoObjeto1 = {
+        ...obj,
+        status: "Ativo",
+        anoEntrada: 2024
+    }
+    
+    console.log(novoObjeto1)
+ }
+ 
+ // Testando a função
+ let aluno = { nome: "João",
+     curso: "ADS" };
+ adicionarInfo(aluno)
+ // Saída esperada: { nome: "João", curso: "ADS", status: "ativo", anoEntrada: 2024 }
+ 
